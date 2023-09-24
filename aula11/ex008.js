e5c03a4a70f82b0235eb4f2972ec3333dc0cbed7
Johnny <johnny_dmg@hotmail.com>
2023-09-24 08:55:33 -0300

    exerciciosproposto

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+console.log(`A velocidade do carro é ${vel} km/h`)
+
+if (vel>=60.5){
+console.log (`Voce ultrapassou o limite de velocidade`)
+}
+
+
+console.log(`Dirija sempre usando cinto`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log("Vivendo em ${pais}")
+if (pais == 'BRASIL'){
+    console.log(' VOCE É BRASILEIRO')
+}else{
+    console.log('VOCE É Estrangeirio')
+}
