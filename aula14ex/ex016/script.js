0136f03ddcbdec5e5ebc1bda14da2491153c375e
Johnny <johnny_dmg@hotmail.com>
2023-09-30 18:54:59 -0300

    updateexercicio

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,19 +1,38 @@
 
 var btn = document.getElementById('btn')
 btn.addEventListener('click', contar)
+//var valid = document.geteElementById('btn')
+//valid.addEventListener('click',validar)
 
-function contar() {
-    //entradas dos inputs
+function contar(){
     var inicio = Number(document.getElementById('inicio').value)
     var fim = Number(document.getElementById('fim').value)
     var passo = Number(document.getElementById('passo').value)
-
     var res = document.getElementById('res')
+
+   if (inicio == "" || fim == "" || passo === ""){
+    //entradas dos inputs
+    res.innerHTML = 'Preencha todos os campos'
+}
+else if(passo==0){
+    passo =1
     var resultados = '';
-
+    
     for (var i = inicio; i <= fim; i += passo) {
-        resultados += `${i}`;  
+        resultados += ` ${i} `;  
     }
-
+    
     res.innerHTML = resultados;
+    window.alert('Passo zero, é considerado 1')
+    }
+    else {
+        var resultados = '';
+        
+        for (var i = inicio; i <= fim; i += passo) {
+            resultados += ` ${i} `;  
+        }
+        
+        res.innerHTML = resultados;
+        
+        }
 }
