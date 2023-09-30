9e935f03e774dfca8f1bc7168c987313d34914a5
Johnny <johnny_dmg@hotmail.com>
2023-09-30 20:47:41 -0300

    melhorando logica

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,35 +4,32 @@ btn.addEventListener('click', contar)
 //var valid = document.geteElementById('btn')
 //valid.addEventListener('click',validar)
 
-function contar(){
+function contar() {
     var inicio = Number(document.getElementById('inicio').value)
     var fim = Number(document.getElementById('fim').value)
     var passo = Number(document.getElementById('passo').value)
     var res = document.getElementById('res')
 
-   if (inicio == "" || fim == "" || passo === ""){
-    //entradas dos inputs
-    res.innerHTML = 'Preencha todos os campos'
-}
-else if(passo==0){
-    passo =1
-    var resultados = '';
-    
-    for (var i = inicio; i <= fim; i += passo) {
-        resultados += ` ${i} `;  
+    if (inicio == "" || fim == "" || passo === "") {
+        //entradas dos inputs
+        res.innerHTML = 'Preencha todos os campos'
     }
-    
-    res.innerHTML = resultados;
-    window.alert('Passo zero, é considerado 1')
+    if (passo <= 0) {
+        passo = 1
+        window.alert('Passo zero, é considerado 1')
     }
-    else {
         var resultados = '';
-        
-        for (var i = inicio; i <= fim; i += passo) {
-            resultados += ` ${i} `;  
+        if (inicio < fim) { //contagem crescente
+            for (var i = inicio; i <= fim; i += passo) {
+                resultados += `\u{1F449} ${i} `
+            }
         }
-        
-        res.innerHTML = resultados;
-        
+        else {   //contagem regressiva
+            for (var i = inicio; i >= fim; i -= passo) {
+                resultados += `\u{1F449} ${i} `
+            }
         }
+        res.innerHTML = `${resultados}\u{1F3C1}`;
+
+    
 }
